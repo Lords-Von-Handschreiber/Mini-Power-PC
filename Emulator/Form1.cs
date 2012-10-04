2bbd4aaad143e67d35567f79c8df83ea2c69a5e9
Thomas Bandixen <thomas@bandixen.ch>
2012-10-04 18:55:29 +0200

    emulator reads file correct

--- Emulator/Form1.cs
+++ Emulator/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +21,17 @@ namespace Emulator
             cpu = new Cpu();
             if (args.Length > 0)
             {
+                var file = new FileInfo(args[0]);
+                byte[] content;
+                using (var br = new BinaryReader(file.OpenRead()))
+                {
+                    content = br.ReadBytes((int)file.Length);
+                }
+
+                for (var i = 0; i < content.Length; i += 2)
+                {
+                    textBox1.Text += Command.ToShort(content[i], content[i+1]).ToString() + Environment.NewLine;
+                }
             }
         }
     }
