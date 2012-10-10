605cc1bba3ab8d914144717407596e4345cb9520
Thomas Bandixen <thomas@bandixen.ch>
2012-10-10 19:21:08 +0200

    designed first gui (only visible)

--- Emulator/Form1.Designer.cs
+++ Emulator/Form1.Designer.cs
@@ -28,21 +28,164 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.listBoxCommandStack = new System.Windows.Forms.ListBox();
+            this.listBoxReg0 = new System.Windows.Forms.ListBox();
+            this.listBoxReg1 = new System.Windows.Forms.ListBox();
+            this.listBoxReg2 = new System.Windows.Forms.ListBox();
+            this.listBoxReg3 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBoxRegCommand = new System.Windows.Forms.ListBox();
+            this.listBox6 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBoxCarry = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
+            // 
+            // listBoxCommandStack
+            // 
+            this.listBoxCommandStack.Enabled = false;
+            this.listBoxCommandStack.FormattingEnabled = true;
+            this.listBoxCommandStack.ItemHeight = 16;
+            this.listBoxCommandStack.Location = new System.Drawing.Point(12, 29);
+            this.listBoxCommandStack.Name = "listBoxCommandStack";
+            this.listBoxCommandStack.Size = new System.Drawing.Size(198, 260);
+            this.listBoxCommandStack.TabIndex = 0;
+            // 
+            // listBoxReg0
+            // 
+            this.listBoxReg0.Enabled = false;
+            this.listBoxReg0.FormattingEnabled = true;
+            this.listBoxReg0.ItemHeight = 16;
+            this.listBoxReg0.Items.AddRange(new object[] {
+            "Akkumulator"});
+            this.listBoxReg0.Location = new System.Drawing.Point(216, 29);
+            this.listBoxReg0.Name = "listBoxReg0";
+            this.listBoxReg0.Size = new System.Drawing.Size(120, 36);
+            this.listBoxReg0.TabIndex = 1;
+            // 
+            // listBoxReg1
+            // 
+            this.listBoxReg1.Enabled = false;
+            this.listBoxReg1.FormattingEnabled = true;
+            this.listBoxReg1.ItemHeight = 16;
+            this.listBoxReg1.Items.AddRange(new object[] {
+            "Register 1"});
+            this.listBoxReg1.Location = new System.Drawing.Point(216, 71);
+            this.listBoxReg1.Name = "listBoxReg1";
+            this.listBoxReg1.Size = new System.Drawing.Size(120, 36);
+            this.listBoxReg1.TabIndex = 2;
+            // 
+            // listBoxReg2
+            // 
+            this.listBoxReg2.Enabled = false;
+            this.listBoxReg2.FormattingEnabled = true;
+            this.listBoxReg2.ItemHeight = 16;
+            this.listBoxReg2.Items.AddRange(new object[] {
+            "Register 2"});
+            this.listBoxReg2.Location = new System.Drawing.Point(216, 113);
+            this.listBoxReg2.Name = "listBoxReg2";
+            this.listBoxReg2.Size = new System.Drawing.Size(120, 36);
+            this.listBoxReg2.TabIndex = 3;
+            // 
+            // listBoxReg3
+            // 
+            this.listBoxReg3.Enabled = false;
+            this.listBoxReg3.FormattingEnabled = true;
+            this.listBoxReg3.ItemHeight = 16;
+            this.listBoxReg3.Items.AddRange(new object[] {
+            "Register 3"});
+            this.listBoxReg3.Location = new System.Drawing.Point(216, 155);
+            this.listBoxReg3.Name = "listBoxReg3";
+            this.listBoxReg3.Size = new System.Drawing.Size(120, 36);
+            this.listBoxReg3.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Speicher Stack";
+            // 
+            // listBoxRegCommand
+            // 
+            this.listBoxRegCommand.Enabled = false;
+            this.listBoxRegCommand.FormattingEnabled = true;
+            this.listBoxRegCommand.ItemHeight = 16;
+            this.listBoxRegCommand.Items.AddRange(new object[] {
+            "Befehlsregister"});
+            this.listBoxRegCommand.Location = new System.Drawing.Point(216, 197);
+            this.listBoxRegCommand.Name = "listBoxRegCommand";
+            this.listBoxRegCommand.Size = new System.Drawing.Size(120, 36);
+            this.listBoxRegCommand.TabIndex = 6;
+            // 
+            // listBox6
+            // 
+            this.listBox6.Enabled = false;
+            this.listBox6.FormattingEnabled = true;
+            this.listBox6.ItemHeight = 16;
+            this.listBox6.Location = new System.Drawing.Point(342, 29);
+            this.listBox6.Name = "listBox6";
+            this.listBox6.Size = new System.Drawing.Size(198, 244);
+            this.listBox6.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(339, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(168, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Speicher [500-529] Stack";
+            // 
+            // checkBoxCarry
+            // 
+            this.checkBoxCarry.AutoSize = true;
+            this.checkBoxCarry.Enabled = false;
+            this.checkBoxCarry.Location = new System.Drawing.Point(216, 239);
+            this.checkBoxCarry.Name = "checkBoxCarry";
+            this.checkBoxCarry.Size = new System.Drawing.Size(96, 21);
+            this.checkBoxCarry.TabIndex = 10;
+            this.checkBoxCarry.Text = "Carry-Flag";
+            this.checkBoxCarry.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1081, 408);
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(872, 420);
+            this.Controls.Add(this.checkBoxCarry);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox6);
+            this.Controls.Add(this.listBoxRegCommand);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBoxReg3);
+            this.Controls.Add(this.listBoxReg2);
+            this.Controls.Add(this.listBoxReg1);
+            this.Controls.Add(this.listBoxReg0);
+            this.Controls.Add(this.listBoxCommandStack);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Mini-Power-PC - Emulator";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBoxCommandStack;
+        private System.Windows.Forms.ListBox listBoxReg0;
+        private System.Windows.Forms.ListBox listBoxReg1;
+        private System.Windows.Forms.ListBox listBoxReg2;
+        private System.Windows.Forms.ListBox listBoxReg3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBoxRegCommand;
+        private System.Windows.Forms.ListBox listBox6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBoxCarry;
 
     }
 }
