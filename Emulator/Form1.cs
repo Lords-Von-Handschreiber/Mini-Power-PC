41b166d662ea93e114cc7f60f91ecb2d7ccc9dcb
Thomas Bandixen <thomas@bandixen.ch>
2012-10-11 18:10:23 +0200

    edited

--- Emulator/Form1.cs
+++ Emulator/Form1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Threading;
 using System.Windows.Forms;
 using Utils;
 
@@ -16,6 +17,7 @@ namespace Emulator
             cpu = new Cpu();
 #if DEBUG
             args = new string[1];
+            args[0] = @"C:\Users\peacemaker\Desktop\Mini-Power-PC.lvhe";
             args[0] = @"C:\Users\Thomas\Dropbox\ZHAW\LVH\Informatik\Semester-3\Aufgaben\Mini Power PC\Mini-Power-PC.lvhe";
 #endif
             if (args.Length > 0)
@@ -31,16 +33,26 @@ namespace Emulator
                 cpu.IsRunnung = true;
                 var t = new System.Threading.Thread(new System.Threading.ThreadStart(delegate
                 {
-                    updateGui();
+                    this.Invoke((MethodInvoker)delegate
+                    {
+                        updateGui(); // runs on UI thread
+                    });
                     System.Threading.Thread.Sleep(750);
 
                     while (cpu.IsRunnung)
                     {
                         cpu.Fetch();
-                        updateGui();
+                        this.Invoke((MethodInvoker)delegate
+                        {
+                            updateGui(); // runs on UI thread
+                        });
+
 
                         cpu.Execute();
-                        updateGui();
+                        this.Invoke((MethodInvoker)delegate
+                        {
+                            updateGui(); // runs on UI thread
+                        });
 
                         System.Threading.Thread.Sleep(50);
                     }
@@ -53,7 +65,9 @@ namespace Emulator
         private void updateGui()
         {
             if (listBoxReg0.Items.Count != 2)
+            {
                 listBoxReg0.Items.Add(cpu.Register[0][0] + " " + cpu.Register[0][1]);
+            }
             else
                 listBoxReg0.Items[1] = cpu.Register[0][0] + " " + cpu.Register[0][1];
 
@@ -86,7 +100,7 @@ namespace Emulator
 
             listBoxCommandStack.Items.Clear();
             var cc = Cpu.ToShort(cpu.CommandCounter);
-            for (var i = (cc - 5*2); i <= (cc + 10*2); i += Cpu.WORD_LENGTH)
+            for (var i = (cc - 5 * 2); i <= (cc + 10 * 2); i += Cpu.WORD_LENGTH)
             {
                 listBoxCommandStack.Items.Add(i + ": " + cpu.FromMemory(i, 1)[0] + " " + cpu.FromMemory(i + 1, 1)[0]);
             }
@@ -100,3 +114,4 @@ namespace Emulator
         }
     }
 }
+
