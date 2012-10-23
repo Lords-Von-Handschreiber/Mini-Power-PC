4e2c464d9dc511bf8e1fbd5b5af1341a79fb7c69
Thomas Bandixen <thomas@bandixen.ch>
2012-10-23 22:13:39 +0200

    modified compile function

--- Utils/Compiler.cs
+++ Utils/Compiler.cs
@@ -29,42 +29,56 @@ namespace Utils
             {
                 using (var w = new System.IO.BinaryWriter(paramFile.OpenWrite()))
                 {
-                    foreach (var line in parameter.Trim().Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries))
+                    foreach (var line in parameter.Trim().Split(Environment.NewLine.ToCharArray()))
                     {
-                        w.Write(Cpu.FromShort(short.Parse(line)));
+                        if (string.IsNullOrEmpty(line.Trim()))
+                        {
+                            w.Write(new byte[2] { 0, 0 });
+                        }
+                        else
+                        {
+                            w.Write(Cpu.FromShort(short.Parse(line)));
+                        }
                     }
                 }
             }
 
             using (var w = new System.IO.BinaryWriter(file.OpenWrite()))
             {
-                foreach (var line in content.Trim().Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries))
+                foreach (var line in content.Trim().Split(Environment.NewLine.ToCharArray()))
                 {
-                    var l = line.Trim().Split(' ');
-                    var cmdString = l[0].Trim();
-                    var args = line.Replace(cmdString, "").Replace(" ", "").Trim(); ;
-
-                    var cmd = Cpu.FromShort((short)Enum.Parse(typeof(Cpu.Cmds), cmdString));
-                    if (!string.IsNullOrEmpty(args))
+                    if (string.IsNullOrEmpty(line.Trim()))
                     {
-                        foreach (var a in args.Split(','))
-                        {
-                            short arg = 0;
-                            if (!a.StartsWith("#"))
-                            {
-                                arg = (short)(short.Parse(a.Trim()) << 10);
-                            }
-                            else
-                            {
-                                arg = short.Parse(a.Replace("#", "").Trim());
-                            }
-                            //var arg = short.Parse(a.Replace("#", "").Trim());
-                            var s = (short)(Cpu.ToShort(cmd) + arg);
-                            cmd = Cpu.FromShort(s);
-                        }
+                        w.Write(new byte[2] { 0, 0 });
                     }
-                    //Console.WriteLine(cmd[1] + "-" + cmd[0] + " = " + cmdString + " " + args + "(" + Command.ToShort(cmd) + ")");
-                    w.Write(cmd);
+                    else
+                    {
+                        var l = line.Trim().Split(' ');
+                        var cmdString = l[0].Trim();
+                        var args = line.Replace(cmdString, "").Replace(" ", "").Trim(); ;
+
+                        var cmd = Cpu.FromShort((short)Enum.Parse(typeof(Cpu.Cmds), cmdString));
+                        if (!string.IsNullOrEmpty(args))
+                        {
+                            foreach (var a in args.Split(','))
+                            {
+                                short arg = 0;
+                                if (!a.StartsWith("#"))
+                                {
+                                    arg = (short)(short.Parse(a.Trim()) << 10);
+                                }
+                                else
+                                {
+                                    arg = short.Parse(a.Replace("#", "").Trim());
+                                }
+                                //var arg = short.Parse(a.Replace("#", "").Trim());
+                                var s = (short)(Cpu.ToShort(cmd) + arg);
+                                cmd = Cpu.FromShort(s);
+                            }
+                        }
+                        //Console.WriteLine(cmd[1] + "-" + cmd[0] + " = " + cmdString + " " + args + "(" + Command.ToShort(cmd) + ")");
+                        w.Write(cmd);
+                    }
                 }
             }
         }
